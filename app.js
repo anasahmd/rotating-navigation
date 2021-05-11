9b6c0f0adcc0fc3600794f89f55a1c6ee262bdb1
Anas Ahmad <76814594+anasahmd@users.noreply.github.com>
2021-05-11 15:12:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const container = document.querySelector('.container');
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const body = document.getElementsByTagName('body')[0];
+const html = document.getElementsByTagName('html')[0];
 
 open.addEventListener('click', () => {
   body.style.overflow = 'hidden';
   window.scrollTo(0, 0);
   container.classList.add('show-nav');
+  html.style.overflow = 'hidden';
 });
 
 close.addEventListener('click', () => {
   container.classList.remove('show-nav');
   body.style.overflow = 'scroll';
+  html.style.overflow = 'scroll';
 });
